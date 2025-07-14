135c114c2c32499daa0fcb904650d98ca0a07119
ahmedghaly15 <ahmedghaly0767@gmail.com>
2025-07-14 22:27:54 +0300

    Built AppNavigator extension

--- lib/src/core/helpers/extensions.dart
+++ lib/src/core/helpers/extensions.dart
@@ -5,3 +5,29 @@ extension ShadThemeAccess on BuildContext {
   ShadThemeData get shadTheme => ShadTheme.of(this);
   ShadTextTheme get shadTextTheme => shadTheme.textTheme;
 }
+
+extension AppNavigator on BuildContext {
+  Future<dynamic> pushNamed(String routeName, {Object? arguments}) {
+    return Navigator.pushNamed(this, routeName, arguments: arguments);
+  }
+
+  Future<dynamic> pushReplacementNamed(String newRoute, {Object? arguments}) {
+    return Navigator.pushReplacementNamed(this, newRoute, arguments: arguments);
+  }
+
+  Future<dynamic> pushNamedAndRemoveUntil(
+    String newRoute, {
+    Object? arguments,
+  }) {
+    return Navigator.pushNamedAndRemoveUntil(
+      this,
+      newRoute,
+      (Route<dynamic> route) => false, // remove all previous routes
+      arguments: arguments,
+    );
+  }
+
+  void pop() => Navigator.pop(this);
+
+  void popTop() => Navigator.of(this, rootNavigator: true).pop();
+}
