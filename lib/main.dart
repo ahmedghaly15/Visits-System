de75cc39b5abdc41a1ad30cce3fcbf446e6d8c70
ahmedghaly15 <ahmedghaly0767@gmail.com>
2025-07-14 22:16:34 +0300

    Updated VisitsSystemApp widget & main method

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart' show ScreenUtil;
 
+import 'src/core/widgets/flutter_error_details_view.dart';
 import 'src/visits_system_app.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await ScreenUtil.ensureScreenSize();
+  ErrorWidget.builder = (FlutterErrorDetails details) =>
+      FlutterErrorDetailsView(details: details);
   runApp(const VisitsSystemApp());
 }
--- lib/src/visits_system_app.dart
+++ lib/src/visits_system_app.dart
@@ -1,12 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:shadcn_ui/shadcn_ui.dart';
+
+import 'core/router/app_router.dart';
+import 'core/utils/const_strings.dart';
 
 class VisitsSystemApp extends StatelessWidget {
   const VisitsSystemApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(body: Center(child: Text('Hello World!'))),
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (_, __) => ShadApp.custom(
+        themeMode: ThemeMode.dark,
+        appBuilder: (_) => MaterialApp(
+          title: ConstStrings.appTitle,
+          debugShowCheckedModeBanner: false,
+          onGenerateRoute: AppRouter.onGenerateRoute,
+          builder: (_, child) => ShadAppBuilder(child: child),
+        ),
+      ),
     );
   }
 }
