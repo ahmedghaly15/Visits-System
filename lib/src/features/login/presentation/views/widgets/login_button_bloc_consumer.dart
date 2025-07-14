feabb835f2bab5321589f47d835e85fbc13aa0dc
ahmedghaly15 <ahmedghaly0767@gmail.com>
2025-07-15 02:42:00 +0300

    Updated listener of LoginButtonBlocConsumer widget

--- lib/src/features/login/presentation/views/widgets/login_button_bloc_consumer.dart
+++ lib/src/features/login/presentation/views/widgets/login_button_bloc_consumer.dart
@@ -2,7 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:visits_system/src/core/helpers/extensions.dart';
 
+import '../../../../../core/api/dio_client.dart';
+import '../../../../../core/helpers/cache_keys.dart';
+import '../../../../../core/helpers/secure_storage_helper.dart';
 import '../../../../../core/locale/locale_keys.dart';
+import '../../../../../core/router/routes.dart';
+import '../../../../../core/utils/functions/check_if_user_is_logged_in.dart';
 import '../../../../../core/widgets/adaptive_circular_progress_indicator.dart';
 import '../../../../../core/widgets/primary_button.dart';
 import '../../cubit/login_cubit.dart';
@@ -27,17 +32,28 @@ class LoginButtonBlocConsumer extends StatelessWidget {
     );
   }
 
-  void _listener(LoginState state, BuildContext context) {
+  void _listener(LoginState state, BuildContext context) async {
     switch (state.status) {
       case LoginStatus.loginLoading:
         context.unfocusKeyboard();
       case LoginStatus.loginSuccess:
-      // TODO: cache token & go to home
+        await _onSuccess(state, context);
       case LoginStatus.loginFailure:
         context.showToast(state.errorMessage!);
       default:
         context.unfocusKeyboard();
     }
+  }
+
+  Future<void> _onSuccess(LoginState state, BuildContext context) async {
+    final newAccessToken = state.loginResponse!.tokens.first.accessToken;
+    DioClient.setTokenIntoHeaders(newAccessToken);
+    accessToken = newAccessToken;
+    await SecureStorageHelper.setSecuredString(
+      CacheKeys.accessToken,
+      newAccessToken,
+    );
+    context.pushReplacementNamed(Routes.home);
   }
 
   bool _listenOrBuildWhen(LoginStatus status) =>
