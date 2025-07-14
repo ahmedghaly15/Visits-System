1cd93eb0842d1094158746441904170e9d2c180e
ahmedghaly15 <ahmedghaly0767@gmail.com>
2025-07-15 02:05:13 +0300

    Provided LoginCubit for LoginView

--- lib/src/core/router/app_router.dart
+++ lib/src/core/router/app_router.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:visits_system/src/core/helpers/extensions.dart';
 
+import '../../../di.dart';
+import '../../features/login/presentation/cubit/login_cubit.dart';
 import '../../features/login/presentation/views/login_view.dart';
 
 class AppRouter {
@@ -9,7 +12,12 @@ class AppRouter {
   static Route<dynamic> onGenerateRoute(RouteSettings settings) {
     switch (settings.name) {
       case '/':
-        return MaterialPageRoute(builder: (context) => const LoginView());
+        return MaterialPageRoute(
+          builder: (_) => BlocProvider<LoginCubit>(
+            create: (_) => getIt.get<LoginCubit>(),
+            child: const LoginView(),
+          ),
+        );
       default:
         return _unFoundRoute();
     }
