cd29958876192524c367029bc116938714fa408c
ahmedghaly15 <ahmedghaly0767@gmail.com>
2025-07-14 22:25:59 +0300

    Built ApiErrorMessage & ApiErrorHandler

---
+++ lib/src/core/api/api_error_message.dart
@@ -0,0 +1,8 @@
+class ApiErrorMessage {
+  ApiErrorMessage._();
+
+  static const String timeoutError =
+      'لم نتمكن من الاتصال بالخادم. حاول مرة أخرى لاحقًا.';
+  static const String noInternetConnection =
+      'لا يوجد اتصال بالإنترنت. يرجى التحقق من الشبكة والمحاولة مرة أخرى.';
+}
