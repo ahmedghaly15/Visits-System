df952a526427cbf1c291931ad322e0f9283c8cf2
ahmedghaly15 <ahmedghaly0767@gmail.com>
2025-07-14 22:08:40 +0300

    Added assets

---
+++ lib/src/core/utils/assets.dart
@@ -0,0 +1,30 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+
+  /// Assets for fontsCairoVariableFontSlnt,wght
+  /// assets/fonts/Cairo-VariableFont_slnt,wght.ttf
+  static const String fontsCairoVariableFontSlntwght =
+      "assets/fonts/Cairo-VariableFont_slnt,wght.ttf";
+
+  /// Assets for fontsREADME
+  /// assets/fonts/README.txt
+  static const String fontsREADME = "assets/fonts/README.txt";
+
+  /// Assets for imagesFoeLogoWhiteCcazrDol
+  /// assets/images/foe-logo-white-CcazrDol.png
+  static const String imagesFoeLogoWhiteCcazrDol =
+      "assets/images/foe-logo-white-CcazrDol.png";
+
+  /// Assets for imagesLogoMiniWhite
+  /// assets/images/logo-mini-white.png
+  static const String imagesLogoMiniWhite = "assets/images/logo-mini-white.png";
+
+  /// Assets for lottieShyLottie
+  /// assets/lottie/shy-lottie.json
+  static const String lottieShyLottie = "assets/lottie/shy-lottie.json";
+
+  /// Assets for svgsLogin
+  /// assets/svgs/login.svg
+  static const String svgsLogin = "assets/svgs/login.svg";
+}
