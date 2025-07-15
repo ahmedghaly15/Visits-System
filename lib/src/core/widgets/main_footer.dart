27792773b805bf21542418ce265419acb374fb93
ahmedghaly15 <ahmedghaly0767@gmail.com>
2025-07-15 07:15:01 +0300

    Updated MainFooter widget

--- lib/src/core/widgets/main_footer.dart
+++ lib/src/core/widgets/main_footer.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:visits_system/src/core/helpers/extensions.dart';
 
 import '../locale/locale_keys.dart';
@@ -10,9 +11,16 @@ class MainFooter extends StatelessWidget {
   Widget build(BuildContext context) {
     return Center(
       child: FittedBox(
-        child: Text(
-          context.translate(LocaleKeys.footer),
-          style: context.shadTextTheme.p,
+        child: Container(
+          padding: EdgeInsets.symmetric(vertical: 8.h, horizontal: 16.w),
+          decoration: BoxDecoration(
+            color: const Color.fromARGB(255, 36, 46, 68),
+            borderRadius: BorderRadius.circular(8.r),
+          ),
+          child: Text(
+            context.translate(LocaleKeys.footer),
+            style: context.shadTextTheme.p,
+          ),
         ),
       ),
     );
